6d1f6683b089a8c735ee9053e0d38c8d56dcc2a9
Gaston Marino <gmarino86@gmail.com>
2022-06-15 00:32:52 -0300

    Creacion de Componentes Evento

---
+++ src/components/Evento/Evento.jsx
@@ -0,0 +1,12 @@
+function Evento(){
+    return (
+        <div>
+            <h1>Evento</h1>
+            <ul>
+                <li>Alguien</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Evento
