1dd279873c3b979eb754dd62e13cf8f9f728c98c
Federico Garabello <it-fede@IT-fede.local>
2022-06-18 15:30:37 -0300

    [HOTFIX][#00] adding fetch and cors

--- back/index.js
+++ back/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import EventosRoute from './routes/eventos.route.js';
+import cors from 'cors'
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
--- front/src/components/Eventos/Eventos.jsx
+++ front/src/components/Eventos/Eventos.jsx
@@ -1,35 +1,23 @@
 import EventosList from "./EventosList";
-const EVENTOS = [
-  {
-    _id: "61887816c49eaaed6b6aee08",
-    titulo: "Futbol con los pibes",
-    id_jugador: [2, 1],
-    lugar: "Costa Rica 5525",
-    deporte: "Futbol",
-    estado: 1,
-    cantParticipantes: 2,
-    dia: "Lunes",
-    hora: "18:00",
-  },
-  {
-    _id: "6188a39f4421f2d9073f6fca",
-    titulo: "Otro evento",
-    id_jugador: [1, 3, 4],
-    lugar: "Cramer 2291",
-    deporte: "Futbol",
-    estado: 1,
-    cantParticipantes: 2,
-    dia: "Jueves",
-    hora: "20:00",
-  },
-];
+import { useState, useEffect } from "react";
+
 
 function Evento() {
+
+  const [eventos, setEventos] = useState([]);
+  
+  useEffect(() => {
+    fetch('http://localhost:3333/api/eventos')
+    .then(res => res.json())
+    .then(data => setEventos(data));
+  }, [])
+
+
   return (
     <div>
       <h1>Eventos</h1>
       <ul>
-        {EVENTOS.map((evento) => (
+        {eventos.map((evento) => (
           <EventosList key={evento._id} event={evento} />
         ))}
       </ul>
